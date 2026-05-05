6fccc3452c4de74626cf78fd5c64e66acaa7395f
madu-montes <monteseduarda56@gmail.com>
2026-05-04 22:07:18 -0300

    feat: lista de candidatos

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const candidatos = [
+    {
+        nome: "Nine Fingers",
+        numero: 31,
+        foto: "./assets/nineFingers.svg"
+    },
+    {
+        nome: "Bolsonaldo",
+        numero: 71,
+        foto: "./assets/bolsonado.svg"
+    },
+    {
+        nome: "Vilma",
+        numero: 71,
+        foto: "./assets/vilma.svg"
+    }
+]
