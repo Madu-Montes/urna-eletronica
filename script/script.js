bea2b5c8d3dadb53357e1c4ca278ffb17e6ab84c
madu-montes <monteseduarda56@gmail.com>
2026-05-05 22:11:08 -0300

    feat: exibir candidato ao digitar numero completo

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const candidatos = [
     {
         nome: "Bolsonaldo",
         numero: 71,
-        foto: "./assets/bolsonado.svg"
+        foto: "./assets/bolsonaldo.svg"
     },
     {
         nome: "Vilma",
@@ -22,9 +22,16 @@ function digitarNumero(numero) {
     numeroDigitado += numero
 
     if (numeroDigitado.length == 2) {
-        console.log("numeroDigitado:", numeroDigitado)
-        let candidatoSelecionado = candidatos.find(candidato => candidato.numero === Number(numeroDigitado))
-        console.log(candidatoSelecionado)
+        console.log("numeroDigitado:", numeroDigitado);
+        let candidatoSelecionado = candidatos.find(candidato => candidato.numero === Number(numeroDigitado));
+
+        console.log(candidatoSelecionado);
+
+        document.getElementById("numero").innerHTML = candidatoSelecionado.numero;
+
+        document.getElementById("foto").src = candidatoSelecionado.foto;
+
+        document.getElementById("nome").innerHTML = candidatoSelecionado.nome;
     }
 
 }
